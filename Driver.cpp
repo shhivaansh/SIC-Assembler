99360ca8071cd2afa99146a6850bb8431c6a6991
shhivaansh <166250589+shhivaansh@users.noreply.github.com>
2025-03-03 21:40:01 +0530

    Add files via upload

---
+++ Driver.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+
+#include "Opcode_map.h"
+#include "Pass1.h"
+#include "Pass2.h"
+
+using namespace std;
+
+int main()
+{
+	cin >> program_name >> str >> starting_address;
+	cin.ignore();
+	input();
+	addressing();
+	generate_object_code();
+	generate_machine_code();	
+}
